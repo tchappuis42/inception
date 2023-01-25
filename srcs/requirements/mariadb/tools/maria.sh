6e49c51af4827736fb879098a7a9dd81ba75a9f8
Tanguy <tanguy.chappuis@gmail.com>
2023-01-26 00:11:01 +0100

    inception

--- srcs/requirements/mariadb/tools/maria.sh
+++ srcs/requirements/mariadb/tools/maria.sh
@@ -1,11 +1,16 @@
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
-mkdir /run/mysqld && chmod 777 /run/mysqld
-mysqld --user=mysql --datadir=/var/lib/mysql &
+#!/bin/sh
+touch $MYSQL_PASSWORD;
+mysql_install_db --user=mysql --datadir=/var/lib/mysql;
+touch $DATABASE_NAME;
+/usr/bin/mysqld --user=mysql  --bootstrap << EOF
 FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;
 USE $DATABASE_NAME;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON *$DATABASE_NAME.* TO '$MYSQL_USER'@'%' IDENTIFIED BY 'test';
+GRANT ALL PRIVILEGES ON $DATABASE_NAME TO '$MYSQL_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
 
-mariadbd-safe
+touch ouiouoi;
+/usr/bin/mysqld;
