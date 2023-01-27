b18a66ac54a875e2e321a92abdf15cbf05f4a4bb
Tanguy <tanguy.chappuis@gmail.com>
2023-01-28 00:46:52 +0100

    inception

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-sleep 10
 mv wp-config.php /var/www/html/
-cd /var/www/html;
-wget http://wordpress.org/latest.tar.gz;
-tar -xzvf latest.tar.gz;
-rm latest.tar.gz;
-chmod 777 wordpress
-mv wordpress/* .
-#rm -rf wordpress
+mv wordpress/* /var/www/html/
+#cd /var/www/html;
+#wget http://wordpress.org/latest.tar.gz;
+#tar -xzvf latest.tar.gz;
+#rm -rf latest.tar.gz
+#chmod 777 wordpress
+#mv wordpress/* .
+rm -rf wordpress/ latest.tar.gz
 
-/usr/sbin/php-fpm7 -F;
+./usr/sbin/php-fpm7 -F;
