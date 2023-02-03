b60e2cb0ccd20979ba2d009d8281e4eef5500f66
Tanguy <tanguy.chappuis@gmail.com>
2023-02-03 17:03:37 +0100

    mariadb

--- srcs/requirements/mariadb/tools/maria.sh
+++ srcs/requirements/mariadb/tools/maria.sh
@@ -1,23 +1,37 @@
 #!/bin/sh
-echo 1
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
-echo 2
-mariadbd-safe --no-watch
-echo 3
-sleep 1
-mysql --user=root -p$MYSQL_ROOT_PASSWORD -e "
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+
+        chown -R mysql:mysql /var/lib/mysql
+
+        # init database
+        mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
+
+        tfile=`mktemp`
+        if [ ! -f "$tfile" ]; then
+                return 1
+        fi
+fi
+if [ ! -d "/var/lib/mysql/wordpress" ]; then
+
+        cat << EOF > $tfile
+USE mysql;
 FLUSH PRIVILEGES;
+DELETE FROM     mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;
-USE $DATABASE_NAME;
-CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $DATABASE_NAME TO '$MYSQL_USER'@'%';
-FLUSH PRIVILEGES;"
-#touch /var/lib/mysql/$MYSQL_ROOT_PASSWORD
-#touch /var/lib/mysql/$DATABASE_NAME
-#touch /var/lib/mysql/$MYSQL_USER
-#touch /var/lib/mysql/1$MYSQL_PASSWORD
-echo 4
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
-echo 5
-mariadbd-safe
+CREATE DATABASE $DATABASE_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$MYSQL_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+	/usr/bin/mysqld --user=mysql --bootstrap < $tfile
+	rm -f $tfile
+fi
+
+sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+
+exec /usr/bin/mysqld --user=mysql --console
--- srcs/requirements/wordpress/wordpress.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-mv wp.config.php /var/www/hmtl/
-cd /var/www/html;
-wget http://wordpress.org/latest.tar.gz;
-tar -xzvf latest.tar.gz;
-rm latest.tar.gz;
-
-/usr/sbin/php-fpm7 -F;
